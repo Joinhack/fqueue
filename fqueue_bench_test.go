035aef0ed7bcbaf4c4cd2cb8e0fbc07296276a26
joinhack <joinhack@gmail.com>
2014-07-31 14:45:17 +0800

    change test

--- fqueue_bench_test.go
+++ fqueue_bench_test.go
@@ -30,10 +30,12 @@ func BenchmarkPush(b *testing.B) {
 	}
 	b.ResetTimer()
 	b.StartTimer()
+	var total int64 = 0
 	for i := 0; i < b.N; i++ {
 		l := rand.Intn(256) + 8
 		p = ptr[:l]
-		b.SetBytes(int64(l))
+		total += int64(len(p))
+		
 		binary.LittleEndian.PutUint32(p, uint32(i))
 		binary.LittleEndian.PutUint32(p[4:], uint32(l))
 
@@ -55,14 +57,16 @@ func BenchmarkPop(b *testing.B) {
 		panic(err)
 	}
 	defer fq.Close()
+	defer os.RemoveAll(fpath)
 	b.ResetTimer()
 	b.StartTimer()
+	var total int64 = 0
 	for i := 0; i < b.N; i++ {
 		if p, err = fq.Pop(); err != nil {
 			b.FailNow()
 			return
 		}
-		b.SetBytes(int64(len(p)))
+		total += int64(len(p))
 		c := int(binary.LittleEndian.Uint32(p))
 		l := int(binary.LittleEndian.Uint32(p[4:]))
 		if c != i || l != len(p) {
--- journalqueue_bench_test.go
+++ journalqueue_bench_test.go
@@ -32,10 +32,11 @@ func BenchmarkJournalQueuePush(b *testing.B) {
 	}
 	b.ResetTimer()
 	b.StartTimer()
+	var total int64 = 0
 	for i := 0; i < b.N; i++ {
 		l := rand.Intn(256) + 8
 		p = ptr[:l]
-		b.SetBytes(int64(l))
+		total += int64(len(p))
 		binary.LittleEndian.PutUint32(p, uint32(i))
 		binary.LittleEndian.PutUint32(p[4:], uint32(l))
 
@@ -57,14 +58,16 @@ func BenchmarkJournalQueuePop(b *testing.B) {
 		panic(err)
 	}
 	defer fq.Close()
+	defer os.RemoveAll(fpath)
 	b.ResetTimer()
 	b.StartTimer()
+	var total int64 = 0
 	for i := 0; i < b.N; i++ {
 		if p, err = fq.Pop(); err != nil {
 			b.FailNow()
 			return
 		}
-		b.SetBytes(int64(len(p)))
+		total += int64(len(p))
 		c := int(binary.LittleEndian.Uint32(p))
 		l := int(binary.LittleEndian.Uint32(p[4:]))
 		if c != i || l != len(p) {
